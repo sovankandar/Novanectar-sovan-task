e478d5beedadf6b8bba0a8f4afb37511acb53b49
sovankandar <157813291+sovankandar@users.noreply.github.com>
2024-07-04 14:38:01 +0530

    Add files via upload

---
+++ khana.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    // Hide the preloader when all resources are loaded
+    var preloader = document.getElementById('preloader');
+    var show= document.getElementById('show');
+    show.style.display = 'block';
+  });
